ad8ad6c0898334e14b4f1ebe760fdcdd2223cc89
ruiming <ruiming.zhuang@gmail.com>
2017-04-22 20:14:59 +0800

    feat(decorator): 添加 jwt 检查装饰器

--- src/decorator/decorator.js
+++ src/decorator/decorator.js
@@ -42,3 +42,22 @@ export function router (path, ...middleware) {
   return function (target, key, descriptor) {
   }
 }
+
+/**
+ * 装饰 route 要求提供 jwt
+ */
+export function authentication () {
+  return (target, key, descriptor) => {
+    descriptor.value = async (ctx, next) => {
+      if (ctx.request.header.authorization && ctx.state.user) {
+        await descriptor.value()
+      } else {
+        ctx.status = 401
+        ctx.body = {
+          success: false,
+          message: 'Permssion denied'
+        }
+      }
+    }
+  }
+}
